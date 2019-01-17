0a019c7a1f73197fea86575a8a138ef387d77d70
barbosavictor <43385570+barbosavictor@users.noreply.github.com>
2019-01-17 16:33:49 -0200

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -6,27 +6,41 @@ echo
 	sudo apt update
 echo
 echo Listas Atualizadas
+sleep 5
+clear
 
 echo Atualizando os Softwares
 echo
 	sudo apt -y upgrade
 echo
 echo Softwares Atualizados
+sleep 5
+clear
+
 
 echo Atualizando o Kernel
 echo
 	sudo apt -y dist-upgrade
 echo
 echo Kernel Atualizado
+sleep 5
+clear
+
 
 echo Removendo Software
 echo
 	sudo apt -y autoremove
 echo
 echo Software Removidos
+sleep 5
+clear
+
 
 echo Limpando o Cache
 echo
 	sudo apt -y autoclean
 echo
 echo Cache Limpo
+sleep 5
+clear
+
